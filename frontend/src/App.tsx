eeadda32f2bc9bd78d57dfbe42ee87cd2606a216
Ishlah Alfath <ishlahfath@gmail.com>
2024-09-17 16:43:51 +0700

    add swal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import PhotoGallery from './components/PhotoGallery';
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
 
+
 const App: React.FC = () => {
   return (
     <div className="App">
